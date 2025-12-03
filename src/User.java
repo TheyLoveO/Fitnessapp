facc5ea4bc4c0cab535bdf606bd2ba0373f8d322
TheyLoveO <123496685+TheyLoveO@users.noreply.github.com>
2025-12-03 07:27:53 -0800

    Delete src directory

--- src/User.java
+++
@@ -1,9 +0,0 @@
-/**
- * Represents a user of the application.
- */
-public class User {
-    final String email;
-    String name;
-    Goal goal = new Goal();
-    User(String email, String name) { this.email = email; this.name = name; }
-}
